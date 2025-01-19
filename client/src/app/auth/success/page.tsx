47df60073bce36c253e9b3033d8fad54c1165c9a
Vladislav Iokhim <vladislaviokhim@Vladislavs-MacBook-Pro.local>
2025-01-19 19:02:27 +0200

    Df

--- client/src/app/auth/success/page.tsx
+++ client/src/app/auth/success/page.tsx
@@ -1,9 +1,21 @@
-import { redirect } from "next/navigation";
+'use client';
 
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+import SuccessPageLoader from "./loading";
 
-const SuccessPage = async () => {
-  await new Promise((resolve) => setTimeout(resolve, 4000));
-  return redirect("/");
+const SuccessPage = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      router.push('/');
+    }, 4000);
+
+    return () => clearTimeout(timer);
+  }, [router]);
+
+  return <SuccessPageLoader />
 };
 
 export default SuccessPage;
