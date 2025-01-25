278914a8d886ae55ed5648b854e3d284bb257f19
Vladislav Iokhim <vladislaviokhim@Vladislavs-MacBook-Pro.local>
2025-01-25 20:24:40 +0200

    dsfdsf

--- client/src/utils/common/index.ts
+++ client/src/utils/common/index.ts
@@ -2,6 +2,8 @@ import { config } from "../config";
 
 const { BACKBLAZEB_BASE_URL, BACKBLAZEB_PUBLIC_BUCKET_NAME } = config;
 
+console.log('config inside utils/common/index.ts',config)
+
 export const generateBackblazeUrl = (folder: string, fileName: string) => {
   return `${BACKBLAZEB_BASE_URL}/${BACKBLAZEB_PUBLIC_BUCKET_NAME}/${folder}/${fileName}`;
 };
