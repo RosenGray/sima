4272d466e99921bf65d90c8b6522fb7df2945c84
Vladislav Iokhim <vladislaviokhim@Vladislavs-MacBook-Pro.local>
2025-02-02 13:02:23 +0200

    d '

--- professionals/dist/events/subjects/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Subjects = void 0;
-var Subjects;
-(function (Subjects) {
-    Subjects["ProfessionalCreated"] = "professional:created";
-})(Subjects || (exports.Subjects = Subjects = {}));
-//# sourceMappingURL=index.js.map
