6835b33cadaf4664533d71b781abfce3b2b38f64
Vladislav Iokhim <vladislaviokhim@Vladislavs-MacBook-Pro.local>
2025-10-03 13:35:59 +0300

    n

--- client/src/lib/professionals/actions/login.ts
+++ client/src/lib/professionals/actions/login.ts
@@ -5,6 +5,4 @@ import { ProfessionalSchema } from "../types/professionals.scema";
 export async function createProfessional(initialState: unknown, formData: FormData) {
   const result = parseWithZod(formData, { schema: ProfessionalSchema });
   if (result.status !== "success") return result.reply();
-
-
 }
