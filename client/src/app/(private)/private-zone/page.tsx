86ef457bf1ed3802843d087e3e262e87d5786f8c
RosenGray <vladonchik@gmail.com>
2026-03-14 12:02:22 +0200

    D

--- client/src/app/(private)/private-zone/page.tsx
+++ client/src/app/(private)/private-zone/page.tsx
@@ -1,14 +1,29 @@
-import { Heading, Text } from "@radix-ui/themes";
+import { Heading, Text, Button } from "@radix-ui/themes";
+import { PlusCircledIcon } from "@radix-ui/react-icons";
+import Link from "next/link";
+import { getCurrentUser } from "@/lib/auth/utils/auth.utils";
+import { WelcomeWrapper, WelcomeDivider } from "./page.styles";
 
-export default function PrivateZonePage() {
+export default async function PrivateZonePage() {
+  const user = await getCurrentUser();
+  const firstName = user?.firstName ?? "Гость";
+
   return (
-    <>
-      {/* <Heading size="6" weight="bold">
-        Личный кабинет
+    <WelcomeWrapper>
+      <Heading size="8" weight="bold">
+        Добро пожаловать,{" "}
+        <span style={{ color: "var(--red-9)" }}>{firstName}</span>!
       </Heading>
       <Text size="3" color="gray">
-        Private zone placeholder
-      </Text> */}
-    </>
+        Здесь вы управляете своими объявлениями и общаетесь с покупателями.
+      </Text>
+      <WelcomeDivider />
+      <Button size="3" variant="solid" color="red" asChild>
+        <Link href="/publish-ad">
+          <PlusCircledIcon />
+          Разместить объявление
+        </Link>
+      </Button>
+    </WelcomeWrapper>
   );
 }
